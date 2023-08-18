6500530d4779b53e2460c2c2ad004b1ad852c5d7
abbyhappying <133288003+abbyhappying@users.noreply.github.com>
2023-08-18 16:34:36 -0700

    Update client3.py

--- client3.py
+++ client3.py
@@ -17,14 +17,17 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price+ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if not price_b:
+        return
+    else:
+        return price_a/price_b
 
 
 # Main
